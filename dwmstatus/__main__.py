d7ae81bfee6397a23d14bc907a150c44515b529e
asafonov <me@asafonov.org>
2016-11-08 11:33:26 +0300

    retrying to get the number of unread email

--- dwmstatus/__main__.py
+++ dwmstatus/__main__.py
@@ -2,14 +2,19 @@ import os, datetime, time
 from dwmstatus.dwmstatus import *
 
 def main():
-    unread = imap_unread()
+    unread = -1
+    num_retry = 0
     while True:
-        status = '[Mail: ' + str(unread if unread >= 0 else '-') + '] '
         today = datetime.datetime.today()
-        status += today.strftime('%Y-%m-%d %H:%M')
-        os.system('xsetroot -name "' + status + '"')
         if int(today.strftime('%M')) % 5 == 0 or unread < 0:
             unread = imap_unread()
+        if unread < 0 and num_retry < 3:
+            num_retry += 1
+            continue
+        num_retry = 0
+        status = '[Mail: ' + str(unread if unread >= 0 else '-') + '] '
+        status += today.strftime('%Y-%m-%d %H:%M')
+        os.system('xsetroot -name "' + status + '"')
         time.sleep(60)
 
 if __name__ == "__main__":
